16d921653e5af6cee02e0cafb201ee2e986265f2
Brett Tanner <bretttanner171@gmail.com>
2023-09-10 18:31:20 +0900

    Create ExperienceFields

--- src/components/ExperienceFields.tsx
+++ src/components/ExperienceFields.tsx
@@ -1,5 +1,34 @@
+import { ChangeEvent, useState } from "react";
+import Field from "./Field";
+
 function ExperienceFields() {
-    return <div></div>
+  const [expDetails, setExpDetails] = useState({
+    company: "",
+    role: "",
+    yearsExperience: "",
+  });
+
+  function onInput(e: ChangeEvent<HTMLInputElement>) {
+    setExpDetails({
+      ...expDetails,
+      [e.currentTarget.id]: e.currentTarget.value,
+    });
+  }
+
+  return (
+    <div className="flex flex-wrap justify-center items-center gap-6">
+      <h1 className="w-full text-center text-3xl font-bold">
+        Experience Information
+      </h1>
+      <Field name="company" value={expDetails.company} onInput={onInput} />
+      <Field name="role" value={expDetails.role} onInput={onInput} />
+      <Field
+        name="yearsExperience"
+        value={expDetails.yearsExperience}
+        onInput={onInput}
+      />
+    </div>
+  );
 }
 
-export default ExperienceFields
+export default ExperienceFields;
